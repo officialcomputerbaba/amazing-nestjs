4a7b123f79c88c83f472e3805e1223cfcfe456d9
Ajit <ajit.thakurs210@gmail.com>
2022-05-29 03:58:20 +0530

    lecture-4-3

--- Fundamentals/lecture-4/src/users.controller.ts
+++ Fundamentals/lecture-4/src/users.controller.ts
@@ -1,47 +1,29 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
 
 @Controller("users")
 export class UsersController {
-  // @Example 1: `string` value as response
-  @Get("ping")
-  sayPong() {
-    return "pong";
-  }
-
-  // @Example 2: `number` value as response
   @Get("followers")
   findFollowers() {
-    return 1000;
+    return ["Amitabh", "Ashish", "Akshay", "Sagar", "Anusha"];
   }
 
-  // @Example 3: `boolean` value as response
-  @Get("online")
-  findIsOnline() {
-    return true;
+  @Post("bio")
+  createUserBio() {
+    return "User bio added";
   }
 
-  // @Example 4: `undefined` or `null` value as response
-  @Get("unknown")
-  findUnknown() {
-    return null; 
-
-    // NOTE: can be `undefined` also
-    // return undefined;
-
-    // NOTE: can be nothing, act as `undefined`
-    // return;
+  @Put("username")
+  changeUsername() {
+    return { success: true };
   }
 
-  // @Example 5: `JSON` as response
-  @Get("me")
-  findMe() {
-    return {
-      user: "Computer Baba Fan",
-      like: true,
-      twitter: "akacomputerbaba",
-    };
+  @Patch("city")
+  updateUserCity() {
+    return { address: { city: "SHDM" } };
+  }
 
-    // NOTE: we can also send an `Array`, treated as `JSON` in response
-    // return ['Ajit', 'Ronaldo', 'Iron Man', 'Dr. Strange']
+  @Delete("photos")
+  deleteUserPhotos() {
+    return; // not returning anything in response (same as `return undefined`)
   }
 }

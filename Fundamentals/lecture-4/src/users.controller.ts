8a5eb24d564e45392ca2566d65b647f767a57da6
Ajit <ajit.thakurs210@gmail.com>
2022-05-29 03:59:39 +0530

    lecture-4-4

--- Fundamentals/lecture-4/src/users.controller.ts
+++ Fundamentals/lecture-4/src/users.controller.ts
@@ -1,29 +1,49 @@
-import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { of } from "rxjs";
+
+function getNetflixShows() {
+  return new Promise((resolve) => {
+    setTimeout(
+      () => resolve({ shows: ["Dark", "Stranger Things", "Elite"] }),
+      5000
+    );
+  });
+}
 
 @Controller("users")
 export class UsersController {
-  @Get("followers")
+  // @Example 1: return a `sync value` that Nest will be able to resolve and send as response
+  @Get("sync")
+  findUsers() {
+    return ["Iron Man", "Dr. Strange"];
+  }
+
+  // @Example 2: return a `promise value` that Nest will be able to resolve and send as response
+  @Get("promise")
   findFollowers() {
-    return ["Amitabh", "Ashish", "Akshay", "Sagar", "Anusha"];
+    return new Promise((resolve, reject) => {
+      setTimeout(
+        () => resolve(["Amitabh", "Ashish", "Akshay", "Sagar", "Anusha"]),
+        5000
+      );
+    });
   }
 
-  @Post("bio")
-  createUserBio() {
-    return "User bio added";
+  // @Example 3: return a `deferred value (async)` that Nest will be able to resolve and send as response
+  @Get("async")
+  async findShows() {
+    const shows = await getNetflixShows();
+
+    return shows;
   }
 
-  @Put("username")
-  changeUsername() {
-    return { success: true };
-  }
-
-  @Patch("city")
-  updateUserCity() {
-    return { address: { city: "SHDM" } };
-  }
-
-  @Delete("photos")
-  deleteUserPhotos() {
-    return; // not returning anything in response (same as `return undefined`)
+  // @Example 4: return a `observable value` that Nest will be able to resolve and send as response
+  @Get("observable")
+  findImages() {
+    return of([
+      "https://picsum.photos/200",
+      "https://picsum.photos/300",
+      "https://picsum.photos/400",
+    ]);
   }
 }

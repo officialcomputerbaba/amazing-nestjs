c3765c8d5634d061b859857b3ee8d1490950abb4
Ajit <ajit.thakurs210@gmail.com>
2022-05-31 02:18:06 +0530

    lecture-6-2

--- Fundamentals/lecture-6/src/users.controller.ts
+++ Fundamentals/lecture-6/src/users.controller.ts
@@ -1,47 +1,42 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 
-interface AlbumParams {
-  id: number;
-  group: string;
+interface AlbumQueryParams {
+  size: number;
+  title: string;
 }
 
 @Controller("users")
 export class UsersController {
-  // @Example 1: Route Params Object
-  @Get("videos/:id")
-  getVideos(@Param() params: Record<string, any>) {
+  // @Example 1: Query Params Object
+  @Get("videos")
+  getVideos(@Query() params: Record<string, any>) {
     console.log(params);
     return params;
   }
 
-  // @Example 2: Multiple params in Route Params Object
-  @Get("movies/:type/:id")
-  getMovies(@Param() params: Record<string, any>) {
-    console.log(params);
-    return params;
+  // @Example 2: Extract `name` param from Query Params Object
+  @Get("shows")
+  getShows(@Query("name") name: string) {
+    console.log(name);
+    return { title: name };
   }
 
-  // @Example 3: Extract `id` param from Route Params Object
-  @Get("shows/:id")
-  getShows(@Param("id") id: number) {
-    // NOTE: `number` data type is just for developer ref, it will not convert the `id` to number
-    console.log(id, typeof id);
-    return { showId: id };
+  // @Example 3: Extract multiple params from Query Params Object
+  @Get("latest-movies")
+  getLatestMovies(
+    @Query("name") name: string,
+    @Query("rating") rating: number // NOTE: data type is only for developer ref, query params values are always in `string` format
+  ) {
+    return { name, rating };
   }
 
-  // @Example 4: Extract multiple params from Route Params Object
-  @Get("latest-movies/:type/:id")
-  getLatestMovies(@Param("type") type: string, @Param("id") id: number) {
-    return { type, movieId: id };
-  }
-
-  // @Example 5: Route Params Object with a `type`
-  @Get("albums/:group/:id")
-  getAlbums(@Param() params: AlbumParams) {
+  // @Example 4: Query Params Object with a `type`
+  @Get("albums")
+  getAlbums(@Query() params: AlbumQueryParams) {
     return {
       message: "You albums group",
-      group: params.group,
-      id: params.id,
+      title: params.title,
+      size: params.size,
     };
   }
 }

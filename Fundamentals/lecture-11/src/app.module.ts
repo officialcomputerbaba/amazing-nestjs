4dea979a65fea250e99c08ffc8621fbf611af31a
Ajit <ajit.thakurs210@gmail.com>
2022-06-11 16:09:27 +0530

    lecture-11-6

--- Fundamentals/lecture-11/src/app.module.ts
+++ Fundamentals/lecture-11/src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from "@nestjs/common";
-import { PersonStore } from "./stores/person.store";
 import { UsersStore } from "./stores/users.store";
 import { UsersController } from "./users.controller";
 
 @Module({
   controllers: [UsersController],
   providers: [
-    UsersStore, // instance 1
-
-    // if we use `useClass` instead of `useExisting` then two instance of `UsersStore` will be created
-    // NOTE: in this case no cached or shared instance of `UsersStore` is used a new one is created that is instance 2
-    { provide: PersonStore, useClass: UsersStore },
+    // NOTE: we are not providing the `UsersStore` dependency
+    // UsersStore,
   ],
 })
 export class AppModule {}
--- Fundamentals/lecture-11/src/users.controller.ts
+++ Fundamentals/lecture-11/src/users.controller.ts
@@ -1,18 +1,16 @@
-import { Controller } from "@nestjs/common";
-import { PersonStore } from "./stores/person.store";
+import { Controller, Optional } from "@nestjs/common";
 import { UsersStore } from "./stores/users.store";
 
 @Controller("/users")
 export class UsersController {
   constructor(
-    private userStore1: UsersStore, // would inject `UserStore` instance 1
-
-    private userStore2: PersonStore // would inject separate instance of  `UserStore` that is instance 2
+    // NOTE: because we are not providing the `UsersStore` dependency
+    // if we don't use Optional then cannot resovle dependencies/provider error is thrown by NestJS
+    @Optional()
+    private userStore: UsersStore
   ) {
     console.log("Inside [UsersController]");
 
-    console.log(this.userStore1.getStore());
-
-    console.log(this.userStore2.getStore());
+    console.log("No instance of UsersStore", this.userStore);
   }
 }

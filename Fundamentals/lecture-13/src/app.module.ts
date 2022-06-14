1397a0aba932f9dfea0773cab3eee1a4eb472f4f
Ajit <ajit.thakurs210@gmail.com>
2022-06-14 23:37:52 +0530

    lecture-13-2

--- Fundamentals/lecture-13/src/app.module.ts
+++ Fundamentals/lecture-13/src/app.module.ts
@@ -1,50 +1,36 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
-import { BehaviorSubject, ReplaySubject } from "rxjs";
-import { EnvConfig, IS_DEV_MODE } from "./config/env.config";
-import { AzureStore, AwsStore, RedisStore } from "./config/stores";
+
+// MOCK: database connection
+function createConnection(options = {}) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        message: "CONNECTED",
+        options,
+      });
+    }, 5000);
+  });
+}
 
 @Module({
   controllers: [UsersController],
   providers: [
-    EnvConfig,
-
     {
-      provide: "MEMORY_STORE",
-      useFactory() {
-        const store = IS_DEV_MODE ? new Map() : new RedisStore();
+      provide: "DATABASE_CONNECTION",
+      // async provider for database connection
+      useFactory: async (options: Record<string, any>) => {
+        const connection = await createConnection(options);
 
-        return store;
+        return connection;
       },
+      inject: ["DB_OPTIONS"],
     },
 
     {
-      provide: "CLOUD_STORE",
-      useFactory: (env: EnvConfig) => {
-        return env.envType == "DEV" ? new AzureStore() : new AwsStore();
-      },
-      inject: [EnvConfig],
+      provide: "DB_OPTIONS",
+      useValue: { url: "localhost", user: "admin", password: "pwd" },
     },
-
-    {
-      provide: "EVENTS_STORE",
-      useFactory: (env: EnvConfig, limit: number = 4) => {
-        const eventBus$ =
-          env.envType === "DEV"
-            ? new ReplaySubject(limit)
-            : new BehaviorSubject(null);
-
-        return eventBus$;
-      },
-      // `EVENTS_LIMIT` is commented out therefore default value `4` is used
-      inject: [EnvConfig, { token: "EVENTS_LIMIT", optional: true }],
-    },
-
-    // NOTE:  'EVENTS_LIMIT` default value will be used by the `EVENTS_STORE` factory
-    // {
-    //   provide: "EVENTS_LIMIT",
-    //   useValue: 2,
-    // },
   ],
 })
 export class AppModule {}

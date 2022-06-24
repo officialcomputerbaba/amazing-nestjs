db359cb1a0d616f785847c026f3ad5be8dae4849
Ajit <ajit.thakurs210@gmail.com>
2022-06-24 22:58:44 +0530

    lecture-16-3

--- Fundamentals/lecture-16/src/employers/employers.module.ts
+++ Fundamentals/lecture-16/src/employers/employers.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { UsersModule } from "src/users/users.module";
 import { EmployersController } from "./controllers/employers.controller";
 import { CompanyService } from "./services/company.service";
 import { EmployersService } from "./services/employers.service";
 
 @Module({
-  imports: [UsersModule],
   controllers: [EmployersController],
   providers: [EmployersService, CompanyService],
   exports: [EmployersService],
--- Fundamentals/lecture-16/src/users/users.module.ts
+++ Fundamentals/lecture-16/src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { AccountsController } from "./controllers/accounts.controller";
 import { UsersController } from "./controllers/users.controller";
 import { UsersService } from "./services/users.service";
 
+@Global()
 @Module({
   controllers: [UsersController, AccountsController],
   providers: [UsersService],

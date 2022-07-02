94c7e347401e47c2e3ea760d52080fcc18351b1f
Ajit <ajit.thakurs210@gmail.com>
2022-07-02 14:32:14 +0530

    lecture-21-2

--- Fundamentals/lecture-21/src/jobs/controllers/jobs.controller.ts
+++ Fundamentals/lecture-21/src/jobs/controllers/jobs.controller.ts
@@ -1,68 +1,39 @@
 import {
-  Body,
-  Controller,
-  ParseUUIDPipe,
-  Get,
-  Param,
-  ParseEnumPipe,
-  ParseArrayPipe,
-  ParseIntPipe,
-  Put,
-  Query,
-  HttpStatus,
-} from "@nestjs/common";
-import { JobsService } from "../services/jobs.service";
-import { JobType } from "../constants/jobs.constants";
-
-@Controller("jobs")
-export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
-
-  // ParseUUIDPipe
-  @Get("ref/:refId")
-  findJobByRefId(@Param("refId", ParseUUIDPipe) id: string) {
-    return this.jobsService.findByRefId(id);
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseEnumPipe,
+    ParseIntPipe,
+    Put,
+    Query,
+    UsePipes,
+  } from "@nestjs/common";
+  import { JobsService } from "../services/jobs.service";
+  import { JobType } from "../constants/jobs.constants";
+  
+  @Controller("jobs")
+  @UsePipes(ParseIntPipe)
+  export class JobsController {
+    constructor(private readonly jobsService: JobsService) {}
+  
+    @Get(":id")
+    findJobById(@Param("id") id: number) {
+      return this.jobsService.findById(id);
+    }
+  
+    @Put("exp/:id")
+    setUpdateJobExp(@Param("id") id: number, @Query("exp") exp: number) {
+      return this.jobsService.setJobExp(id, exp);
+    }
+  
+    // Order of execution = (Controller Level) ParseIntPipe -> (Method Arg Level `Body`) ParseEnumPipe
+    @Put("type/:id")
+    toggleJobType(
+      @Param("id") id: number,
+      @Body("type", new ParseEnumPipe(JobType)) type: JobType
+    ) {
+      return this.jobsService.toggleJobType(id, type);
+    }
   }
-
-  // ParseUUIDPipe - UUID version 3
-  @Get("ref-v3/:refId")
-  findJobByUUID3RefId(
-    @Param("refId", new ParseUUIDPipe({ version: "3" })) id: string
-  ) {
-    return this.jobsService.findByRefId(id);
-  }
-
-  // ParseUUIDPipe - UUID version 4
-  @Get("ref-v4/:refId")
-  findJobByUUID4RefId(
-    @Param(
-      "refId",
-      new ParseUUIDPipe({
-        version: "4",
-        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
-      })
-    )
-    id: string
-  ) {
-    return this.jobsService.findByRefId(id);
-  }
-
-  // ParseEnumPipe
-  @Put("type/:id")
-  toggleJobType(
-    @Param("id", ParseIntPipe) id: number,
-    @Body("type", new ParseEnumPipe(JobType)) type: JobType
-  ) {
-    return this.jobsService.toggleJobType(id, type);
-  }
-
-  // ParseArrayPipe
-  // NOTE: we need to install the class-validator & class-transformer packages to use the `ParseArrayPipe`
-  @Get("search")
-  findJobsByIds(
-    @Query("id", new ParseArrayPipe({ items: Number, separator: "," }))
-    ids: number[]
-  ) {
-    return this.jobsService.findByIds(ids);
-  }
-}
+  

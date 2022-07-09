b6211f077a2647459351394d14e32775da79f2cc
Ajit <ajit.thakurs210@gmail.com>
2022-07-10 04:31:56 +0530

    lecture-25-6

--- Fundamentals/lecture-25/src/app.module.ts
+++ Fundamentals/lecture-25/src/app.module.ts
@@ -1,7 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
 import { JobsModule } from "./jobs/jobs.module";
 
+console.log("[app.module.ts]: `APP_PIPE` with `useClass` ValidationPipe");
 @Module({
   imports: [JobsModule],
+  // NOTE: we can also use this technique to make the ValidationPipe global
+  //   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class AppModule {}
--- Fundamentals/lecture-25/src/main.ts
+++ Fundamentals/lecture-25/src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log("[main.ts]: Global ValidationPipe");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();

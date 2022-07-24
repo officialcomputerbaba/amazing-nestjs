efc316fb32726be5407f75f195fc5bdc1ef66bba
Ajit <ajit.thakurs210@gmail.com>
2022-07-24 12:35:49 +0530

    lecture-29-2

--- Fundamentals/lecture-29/src/jobs/controllers/jobs.controller.ts
+++ Fundamentals/lecture-29/src/jobs/controllers/jobs.controller.ts
@@ -4,20 +4,17 @@ import {
   Get,
   Param,
   ParseIntPipe,
-  UseFilters,
 } from "@nestjs/common";
 
 import { IdException } from "../../exceptions/id-exception";
 import { JobsService } from "../services/jobs.service";
-import { AppExceptionFilter } from "src/exceptions/app-exception.filter";
 
 @Controller("jobs")
-@UseFilters(AppExceptionFilter) // any kind of exception is handle by `AppExceptionFilter`
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Get(":id")
-  // both exceptions are handle by `AppExceptionFilter` defined on Controller
+  // both exceptions are handle by `AppExceptionFilter` marked global in `main.ts`
   findJobById(@Param("id", ParseIntPipe) id: number) {
     if (id <= 0) {
       throw new IdException("Invalid id");

ca5245f45fda7473c2882a9683c8893dec37157e
Ajit <ajit.thakurs210@gmail.com>
2022-08-06 03:21:31 +0530

    lecture-32-4

--- Fundamentals/lecture-32/src/jobs/jobs.module.ts
+++ Fundamentals/lecture-32/src/jobs/jobs.module.ts
@@ -22,62 +22,10 @@ import {
   })
   export class JobsModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
-      // @example 1: single route
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes("jobs");
-  
-      // -----
-  
-      // @example 2: multiple controller routes
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes("jobs", "interviews");
-  
-      // -----
-  
-      // @example 3: nested route(s)
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes("jobs/refs");
-  
-      // -----
-  
-      // @example 4: wildcards | r*fs -> refs, rafs, rmfs
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes("jobs/r*fs");
-  
-      // -----
-  
-      // @example 5: wildcards | refs? -> ref, refs
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes("jobs/refs?");
-  
-      // -----
-  
-      // @example 6: route controller(s)
-  
-      // consumer.apply(UserAgentMiddleware).forRoutes(JobsController);
-      // consumer
-      //   .apply(UserAgentMiddleware)
-      //   .forRoutes(JobsController, InterviewsController);
-  
-      // -----
-  
-      // @example 7: RouteInfo Object(s)
-  
-      // consumer
-      //   .apply(UserAgentMiddleware)
-      //   .forRoutes({ path: "jobs/refs", method: RequestMethod.POST });
-  
-      // -----
-  
-      // @example 8: Mixed Configs
-  
       consumer
         .apply(UserAgentMiddleware)
-        .forRoutes(
-          { path: "jobs/refs", method: RequestMethod.POST },
-          InterviewsController,
-          "office"
-        );
+        .exclude({ path: "jobs/refs", method: RequestMethod.POST })
+        .forRoutes("jobs");
     }
   }
   

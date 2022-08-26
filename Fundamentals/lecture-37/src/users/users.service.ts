8edf70eed815409faf42ce6cfc039c4335685877
Ajit <ajit.thakurs210@gmail.com>
2022-08-27 00:54:40 +0530

    lecture-37-2

--- Fundamentals/lecture-37/src/users/users.service.ts
+++ Fundamentals/lecture-37/src/users/users.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
+interface JwtConfig {
+  "JWT.SECRET": string;
+  "JWT.EXPIRE_TIME": number;
+}
+
 @Injectable()
 export class UsersService {
-  constructor(private readonly configService: ConfigService) {
+  constructor(private readonly configService: ConfigService<JwtConfig>) {
     // jwt config
     const secret = this.configService.get<string>("JWT.SECRET");
     const expireTime = this.configService.get("JWT.EXPIRE_TIME");
 
-    console.log("\n[UsersService]: Jwt config");
+    console.log("\n[UsersService]: Jwt config with an Interface | Configuration Data Type");
     console.log(secret, expireTime);
   }
 }

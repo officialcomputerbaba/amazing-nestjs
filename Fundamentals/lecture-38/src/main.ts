de7ee02c7f56d802de789c3abb2d6f29c9e8f5ea
Ajit <ajit.thakurs210@gmail.com>
2022-09-03 23:37:04 +0530

    lecture-38-2

--- Fundamentals/lecture-38/src/main.ts
+++ Fundamentals/lecture-38/src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-      httpsOptions: {
-        key: readFileSync(resolve(__dirname, "../cert/private.key")),
-        cert: readFileSync(resolve(__dirname, "../cert/public.cert")),
-      },
-    });
+  const httpsOptions = {
+    key: readFileSync(resolve(__dirname, "../cert/private.key")),
+    cert: readFileSync(resolve(__dirname, "../cert/public.cert")),
+  };
 
-    await app.listen(443);
+  const app = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter({
+      https: httpsOptions,
+    })
+  );
+
+  await app.listen(443);
 }
 bootstrap();

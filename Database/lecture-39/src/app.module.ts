a30ba0f5e84b82c18fa6cb3da697a512add0d2e4
Ajit <ajit.thakurs210@gmail.com>
2022-09-17 16:28:07 +0530

    lecture-39-3

--- Database/lecture-39/src/app.module.ts
+++ Database/lecture-39/src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    // NOTE: set the remote instance username, password, and host parameter
-    MongooseModule.forRoot(
-      `mongodb+srv://${username}:${password}@${host}/nest_app_db?retryWrites=true&w=majority`
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        const username = config.get("DATABASE_USER");
+        const password = config.get("DATABASE_PASSWORD");
+        const host = config.get("DATABASE_HOST");
+        const port = config.get("DATABASE_PORT");
+        const db = config.get("DATABASE_NAME");
+        const isLocal = config.get("NODE_ENV") === "LOCAL";
+
+        const uri = isLocal
+          ? `mongodb://localhost:${port}/${db}`
+          : `mongodb+srv://${username}:${password}@${host}/${db}?retryWrites=true&w=majority`;
+
+        return {
+          uri,
+          // NOTE: we can other options as well, below are the some options you can set
+          //   useNewUrlParser: true,
+          //   useUnifiedTopology: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
   ],
 })
 export class AppModule {}
